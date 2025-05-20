a230c3a1e92888dcc4de085b254b8149bf636d59
Josua <148541326+J0su4K@users.noreply.github.com>
2025-05-20 16:16:52 +0200

    Update : Ajout du C++(pour l'UI) , UI+HUD+GameMode

---
+++ bayrou/Source/bayrou/UW_MainMenu.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BAYROU_API UUW_MainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+//	
+//public:
+//	virtual void BeginPlay() override;
+};
